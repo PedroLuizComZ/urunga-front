af5d1a296c4c314e81f1d2d793d315e2b7ea7527
Pedro Silva <pe_lfsilva@hotmail.com>
2024-01-10 20:34:10 -0300

    Add meta tag type robot

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,6 +9,8 @@ export default function Document() {
           href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css"
         />
         <meta name="description" content="Urunga" />
+        <meta name="robots" content="index, follow" />
+
         <title>Urunga</title>
       </Head>
       <body>
